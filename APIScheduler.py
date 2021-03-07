3f2aa782986a6892893a4bed51e25228820c3d74
abby3010 <abhayubhale.30@gmail.com>
2021-03-07 16:29:11 +0530

    first commit

--- APIScheduler.py
+++ APIScheduler.py
@@ -11,11 +11,6 @@ db = firestore.client()
 
 sched = BackgroundScheduler(daemon=True)
 
-@sched.add_job('interval', minutes=3)
-def timed_job():
-    db.collection(u'Users').document(u'abhayubhale.30@gmail.com').update({
-        "listTimed": firestore.ArrayUnion([datetime.datetime.now()])
-    })
 
 # @sched.scheduled_job('cron', day_of_week='mon-sun', hour=10)
 @sched.add_job('cron', day='*', hour='*', minute='2')
