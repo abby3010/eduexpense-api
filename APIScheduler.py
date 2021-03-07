3dff6558b0e2af2d316719c35748b48c4031dcb7
abby3010 <abhayubhale.30@gmail.com>
2021-03-07 16:43:21 +0530

    CRON JOB ADDED

--- APIScheduler.py
+++ APIScheduler.py
@@ -11,11 +11,18 @@ db = firestore.client()
 
 sched = BackgroundScheduler(daemon=True)
 
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    db.collection(u'Users').document(u'abhayubhale.30@gmail.com').update({
+        "listTimed": firestore.ArrayUnion([datetime.datetime.now()])
+    })
+
+# @sched.scheduled_job('cron', day_of_week='mon-sun', hour=10)
+@sched.scheduled_job('cron', day='*', hour='*', minute='2')
 def scheduled_job():
-    
+    ''' The functions run to call data from APIs and is scheduled to run only once in a day '''
     db.collection(u'Users').document(u'abhayubhale.30@gmail.com').update({
         "count": firestore.Increment(1)
     })
 
-sched.add_job(scheduled_job, 'cron', day='*', hour='*', minute='2')
 sched.start()
