d56242cdb80f70ac421d6dc8af4987e274f5f6d6
abby3010 <abhayubhale.30@gmail.com>
2021-03-07 16:19:04 +0530

    first commit

--- APIScheduler.py
+++ APIScheduler.py
@@ -1,13 +1,28 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+import firebase_admin
+from firebase_admin import credentials, firestore
+import datetime
+
+# Connect to database
+cred = credentials.Certificate('credentials.json')
+default_app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
-    print('This job is run every three minutes.')
+    db.collection(u'Users').document(u'abhayubhale.30@gmail.com').update({
+        "listTimed": firestore.ArrayUnion([datetime.datetime.now()])
+    })
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=10, )
+# @sched.scheduled_job('cron', day_of_week='mon-sun', hour=10)
+@sched.scheduled_job('cron', day='*', hour='*', minute='2')
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    
+    db.collection(u'Users').document(u'abhayubhale.30@gmail.com').update({
+        "count": firestore.Increment(1)
+    })
 
 sched.start()
--- main.py
+++ main.py
@@ -1,39 +1,10 @@
 from flask import Flask
-from apscheduler.schedulers.background import BackgroundScheduler
-import firebase_admin
-from firebase_admin import credentials, firestore
-import datetime
 
-# from flask_apscheduler import APScheduler
 app = Flask(__name__)
-
-# scheduler instance
-sched = BackgroundScheduler(daemon=True)
-
-
-# Connect to database
-cred = credentials.Certificate('credentials.json')
-default_app = firebase_admin.initialize_app(cred)
-db = firestore.client()
-
-
-def increment_counter():
-    ''' The functions run to call data from APIs and is scheduled to run only once in a day '''
-    db.collection(u'Users').document(u'abhayubhale.30@gmail.com').update({
-        "count": firestore.Increment(1)
-    })
-
-    print("Counter Incremented")
-
-
-def add_to_list():
-    db.collection(u'Users').document(u'abhayubhale.30@gmail.com').update({
-        "list": firestore.ArrayUnion([datetime.datetime.now()])
-    })
 
 @app.route('/')
 def index():
-    return "Hola Amigo!"
+    return "This is Coding Abby"
 
 
 if __name__ == '__main__':
@@ -41,6 +12,6 @@ if __name__ == '__main__':
     # sched.add_job(increment_counter, 'cron', day='*', hour=14, minute=50, second=10)
     # sched.add_job(increment_counter, 'cron', day='*', hour=14, minute=50, second=15)
     # sched.add_job(increment_counter, 'cron', day='*', hour=14, minute=50, second=20)
-    sched.add_job(add_to_list, 'cron', day='*', hour='*', minute='5')
-    sched.start()
+    # sched.add_job(add_to_list, 'cron', day='*', hour='*', minute='5')
+    # sched.start()
     app.run()
