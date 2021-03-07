d4174c6af8e0b4850a78c2399128d5f747e66bee
abby3010 <abhayubhale.30@gmail.com>
2021-03-07 16:26:55 +0530

    first commit

--- APIScheduler.py
+++ APIScheduler.py
@@ -1,4 +1,4 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -9,16 +9,16 @@ cred = credentials.Certificate('credentials.json')
 default_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-sched = BlockingScheduler()
+sched = BackgroundScheduler(daemon=True)
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.add_job('interval', minutes=3)
 def timed_job():
     db.collection(u'Users').document(u'abhayubhale.30@gmail.com').update({
         "listTimed": firestore.ArrayUnion([datetime.datetime.now()])
     })
 
 # @sched.scheduled_job('cron', day_of_week='mon-sun', hour=10)
-@sched.scheduled_job('cron', day='*', hour='*', minute='2')
+@sched.add_job('cron', day='*', hour='*', minute='2')
 def scheduled_job():
     
     db.collection(u'Users').document(u'abhayubhale.30@gmail.com').update({
