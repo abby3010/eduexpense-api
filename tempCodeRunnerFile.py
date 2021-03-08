188f8d4c88c2c2927b56bade1bac38b805619062
abby3010 <abhayubhale.30@gmail.com>
2021-03-08 23:20:30 +0530

    Finance Data is getting fetched and stored in databse

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-
-# scheduler.add_job(get_silver_rates, 'cron', day_of_week='mon-sun', hour='10', minute='35', timezone=tz)
-# scheduler.add_job(get_gold_rates, 'cron', day_of_week='mon-sun', hour='10', minute='36', timezone=tz)
