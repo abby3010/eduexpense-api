df38177ee9cf0eab85c30a0452ca8d1b9708da99
abby3010 <abhayubhale.30@gmail.com>
2021-03-08 19:19:40 +0530

    Finance Data is getting fetched and stored in databse

--- cronjobs.py
+++ cronjobs.py
@@ -1,30 +1,20 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-
-import firebase_admin
-from firebase_admin import credentials, firestore
+from getData import *
 import datetime
+import pytz
 
-# Connect to database
-cred = credentials.Certificate('credentials.json')
-default_app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+# Stock markets both NSE and BSE
+# TIMING:  9:15 A.M. to 3:30 P.M
+# DAYS: Monday to Friday
 
-
-def timed_job():
-    db.collection(u'Users').document(u'abhayubhale.30@gmail.com').update({
-        "listTimed": firestore.ArrayUnion([datetime.datetime.now()])
-    })
-    print("Timestamp added to FIREBASE")
-
-def scheduled_job():
-    ''' The functions run to call data from APIs and is scheduled to run only once in a day '''
-    db.collection(u'Users').document(u'abhayubhale.30@gmail.com').update({
-        "count": firestore.Increment(1)
-    })
-    print("Counter Increased in FIREBASE")
 
 scheduler = BlockingScheduler()
 
-scheduler.add_job(timed_job, "interval", minutes=30)
-scheduler.add_job(scheduled_job, 'cron', day='*', hour='*', minute='30')
+tz = pytz.timezone('Asia/Kolkata')
+
+scheduler.add_job(get_silver_rates, 'cron', day_of_week='mon-sun', hour='10', minute='35', timezone=tz)
+scheduler.add_job(get_gold_rates, 'cron', day_of_week='mon-sun', hour='10', minute='36', timezone=tz)
+scheduler.add_job(get_sensex, 'cron', day_of_week='mon-fri', hour='9-16', minute='*/5', timezone=tz)
+scheduler.add_job(get_nifty, 'cron', day_of_week='mon-fri', hour='9-16', minute='*/5', timezone=tz)
 scheduler.start()
+print("Program Executed")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+
+# scheduler.add_job(get_silver_rates, 'cron', day_of_week='mon-sun', hour='10', minute='35', timezone=tz)
+# scheduler.add_job(get_gold_rates, 'cron', day_of_week='mon-sun', hour='10', minute='36', timezone=tz)
